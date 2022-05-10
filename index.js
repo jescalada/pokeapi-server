34c7f628b54df84d946ad8b923d047503ad6efbe
Juan Escalada <juanescalada175@gmail.com>
2022-05-10 13:09:58 -0700

    Add mongoose packages and methods

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express')
+const bodyparser = require("body-parser");
+const mongoose = require('mongoose');
+const path = require('path');
+
 const app = express()
 const port = 3000
 
-const path = require('path');
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+const pokemonSchema = new mongoose.Schema({
+    name: String,
+    types: [String],
+    abilities: [String],
+    id: Number,
+    stats: [Object],
+    sprite: String
+});
+
+const pokemonModel = mongoose.model("pokemon", pokemonSchema);
+
+mongoose.connect("mongodb+srv://juan:Rocco123@cluster0.nxfhi.mongodb.net/pokemon-db?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
