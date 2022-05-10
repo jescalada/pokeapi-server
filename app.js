e2046cb7dd55064b19d77e0ccf764d513865c0da
Juan Escalada <juanescalada175@gmail.com>
2022-05-10 15:31:27 -0700

    Fix port binding bug on deploy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyparser.urlencoded({
     extended: true
